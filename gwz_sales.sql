69e3f9d92435896634bc5153e469da0142f3d35f
PsychicDonkey <thomvonzahn1@gmail.com>
2024-07-09 20:29:53 +0200

    Update gwz_sales.sql
    sort dates DESC

--- gwz_sales.sql
+++ gwz_sales.sql
@@ -5,6 +5,12 @@ SELECT
   , ROUND(SUM(purchase_cost),2) as cost_daily
   , ROUND(SUM(turnover - purchase_cost),2) as profit_daily
 FROM `data-analytics-bootcamp-363212.course14.gwz_sales` 
+<<<<<<< Updated upstream
 GROUP BY date_date
 ORDER BY date_date ASC
 LIMIT 1000
+=======
+GROUP BY date_date --- by day
+ORDER BY date_date DESC
+LIMIT 1000 --- max last three years 
+>>>>>>> Stashed changes
